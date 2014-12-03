1662f57021f33d50646e75eab0d27722765637aa
moisessiles <moises.siles@gmail.com>
2014-12-02 22:03:29 -0600

    Refactor Fix test failures Remove the OpenHome from Home class

--- tests/login_tests.py
+++ tests/login_tests.py
@@ -2,27 +2,24 @@ __author__ = 'moisessiles'
 
 
 from commons.testBase import TestBase
-from pages.home import HomePage
+from pages.utils import Utilities
 from pages.login import LoginPage
-import time
 
 
 class LoginTests(TestBase):
 
     def test_wrong_login_verification(self):
-        home_pg = HomePage(self.selenium)
-        home_pg.go_to_home_page()
+        home_pg = Utilities.open_home_page(self.selenium)
         login_pg = LoginPage(self.selenium)
         home_pg.go_to_login_page()
         login_pg.login_as_wrong_user()
 
-        #TODO Add Validations
+        assert login_pg.error_message() == "The username or password provided is incorrect.", "Error"
 
     def test_successful_login_verification(self):
-        home_pg = HomePage(self.selenium)
-        home_pg.go_to_home_page()
+        home_pg = Utilities.open_home_page(self.selenium)
         login_pg = LoginPage(self.selenium)
         home_pg.go_to_login_page()
         login_pg.login_successful("moises.siles@gmail.com", "123456")
 
-        #TODO Add Validations
+        assert login_pg.welcome_message() == "moises.siles@gmail.com", "Error"
