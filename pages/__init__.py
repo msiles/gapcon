78c9d232b0d1320c95f58ed00f4fdef74dd3dbd6
moisessiles <moises.siles@gmail.com>
2014-12-08 07:06:09 -0600

    Revert d0335a5..d5ae398
    This rolls back to commit d0335a5dab7291ce1c1dbdcd8ce14b90f0eb5a2f.

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,1 +1,8 @@
 __author__ = 'moisessiles'
+
+if __name__ == "__main__":
+    config = {}
+    execfile("config.cfg", config)
+    # python 3: exec(open("example.conf").read(), config)
+
+    print config["url"]
