d5ae3985ff502a0e1392a4de6e4b609f2754405f
moisessiles <moises.siles@gmail.com>
2014-12-08 07:03:08 -0600

    Revert "Refactor"
    This reverts commit d0335a5dab7291ce1c1dbdcd8ce14b90f0eb5a2f.

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,8 +1,1 @@
 __author__ = 'moisessiles'
-
-if __name__ == "__main__":
-    config = {}
-    execfile("config.cfg", config)
-    # python 3: exec(open("example.conf").read(), config)
-
-    print config["url"]
