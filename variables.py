6fc1d90e088aad09773241c673ef7be8d02515fa
moisessiles <moises.siles@gmail.com>
2014-12-08 07:06:25 -0600

    Revert 1662f57..78c9d23
    This rolls back to commit 1662f57021f33d50646e75eab0d27722765637aa.

---
+++ variables.py
@@ -0,0 +1,9 @@
+__author__ = 'msiles'
+from selenium import webdriver
+
+
+_protocol = "https"
+#_url = "dev-big-play.azure-mobile.net"
+_url = "preview-big-play.azure-mobile.net"
+
+_testing_url = "{}://{}/api/" .format(_protocol, _url)
