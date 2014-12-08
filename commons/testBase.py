d0335a5dab7291ce1c1dbdcd8ce14b90f0eb5a2f
moisessiles <moises.siles@gmail.com>
2014-12-08 06:22:48 -0600

    Refactor Fix test failures Remove the OpenHome from Home class

--- commons/testBase.py
+++ commons/testBase.py
@@ -1,11 +1,18 @@
 __author__ = 'msiles'
 
-
+import os
+import sys
+import new
 import unittest
 from selenium import webdriver
+from sauceclient import SauceClient
 
 
 class TestBase(unittest.TestCase):
+
+    USERNAME = os.environ.get('SAUCE_USERNAME', "msiles")
+    ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY', "3d1aec67-5936-4898-ba6b-0bc50e8990ba")
+    sauce = SauceClient(USERNAME, ACCESS_KEY)
 
     def setUp(self):
         self.selenium = webdriver.Chrome()
--- variables.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'msiles'
-from selenium import webdriver
-
-
-_protocol = "https"
-#_url = "dev-big-play.azure-mobile.net"
-_url = "preview-big-play.azure-mobile.net"
-
-_testing_url = "{}://{}/api/" .format(_protocol, _url)
